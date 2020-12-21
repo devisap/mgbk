ab3b8386270220e6862ac123a9ae9209704af650
devisap <ilham.sagitaputra@gmail.com>
2020-12-21 23:59:29 +0700

    install & create structure project & install reactsvg

---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import IconEmail from '../../assets/icons/ic_email.svg';
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>
+                <IconEmail />
+            </Text>
+        </View>
+    );
+}
+
+export default SplashScreen;
